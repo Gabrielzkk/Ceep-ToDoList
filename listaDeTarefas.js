b3ad7999ea962aad468d177b64b04246db9c5790
Gabriel Succi <zicanony@gmail.com>
2021-07-12 14:52:13 -0300

    Adicionando um Evento Escutador no button.

---
+++ listaDeTarefas.js
@@ -0,0 +1,8 @@
+const addNovaTarefa = document.querySelector("[data-form-button]");
+
+addNovaTarefa.addEventListener("click", (event) => {
+
+    event.preventDefault();
+    console.log("teste de clique");
+});
+console.log(addNovaTarefa)
