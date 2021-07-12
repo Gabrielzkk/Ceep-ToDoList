44a18f60c816f8945dc242daf5466d91dc3cb699
Gabriel Succi <zicanony@gmail.com>
2021-07-12 16:51:25 -0300

    Estilizando e adicionando efeitos visuais ao botão.

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -29,10 +29,20 @@ addNovaTarefa.addEventListener("click", criarTarefa);
 const BotaoCheck = () => {
 
     const botaoCheck = document.createElement("button");
+    botaoCheck.classList.add("check-button");
+    botaoCheck.innerText = "Concluído";
 
-    botaoCheck.addEventListener("click", () => {
-        console.log("Tarefa concluída!")
-    });
+    botaoCheck.addEventListener("click", concluirTarefa);
 
     return botaoCheck;
+}
+
+
+const concluirTarefa = function(event){
+
+    const botaoCheck = event.target;
+    const tarefaCompleta = botaoCheck.parentElement;
+
+    tarefaCompleta.classList.toggle("done");
+    console.log("Tarefa concluída");
 }
