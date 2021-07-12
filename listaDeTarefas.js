6542df6e9ba90df436955cec8d016df189a80faa
Gabriel Succi <zicanony@gmail.com>
2021-07-12 15:36:24 -0300

    Adicionando na tabela.

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,18 @@
 const criarTarefa = (event) => {
 
     event.preventDefault();
+
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valorInput = input.value;
+
+
+    const tarefa = document.createElement("li");
+    const conteudo = `<p class="content">${valorInput}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
+
     input.value = "";
 }
 
